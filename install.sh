88e336cf7cec7123e3f56d288a3967b3469a8382
Peter Mousses <29151865+petermousses@users.noreply.github.com>
2024-10-12 04:11:26 -0700

    Removed accidental shell call

--- install.sh
+++ install.sh
@@ -15,5 +15,4 @@ nix-build https://github.com/LnL7/nix-darwin/archive/master.tar.gz -A installer
 nix flake init -t nix-darwin --extra-experimental-features "nix-command flakes"
 sed -i '' "s/simple/$(scutil --get LocalHostName)/" flake.nix
 
-wd=$(pwd)
-nix-shell run nix-darwin --extra-experimental-features "nix-command flakes" -- switch --flake .#$(scutil --get LocalHostName)
+nix run nix-darwin --extra-experimental-features "nix-command flakes" -- switch --flake .#$(scutil --get LocalHostName)
