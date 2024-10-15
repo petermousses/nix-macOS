396a7be354f5940c194da32bef966f17da2735c6
Peter Mousses <29151865+petermousses@users.noreply.github.com>
2024-10-15 01:02:03 -0700

    Fixed rebuilding

--- update.sh
+++ update.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-# darwin-rebuild switch --flake .#$(scutil --get LocalHostName) --extra-experimental-features "nix-command flakes"
-nix run nix-darwin --extra-experimental-features "nix-command flakes" -- switch --flake .#$(scutil --get LocalHostName)
+darwin-rebuild switch --flake .#$(scutil --get LocalHostName)
