ddbcd36f91a65414879132c24dbb5d26c053a2b7
Peter Mousses <29151865+petermousses@users.noreply.github.com>
2024-10-16 08:19:25 -0700

    allow nix commands to be run after install script

--- install.sh
+++ install.sh
@@ -4,8 +4,8 @@
 
 # This command needs the sudo password and other inputs, so it is commented for now
 # Install Nix package manager. Source: https://nixos.org/download
-# echo "Installing Nix package manager"
-# sh <(curl -L https://nixos.org/nix/install)
+echo "Installing Nix package manager"
+sh <(curl -L https://nixos.org/nix/install)
 
 conf_dir="~/.config/nix-darwin"
 
@@ -17,6 +17,7 @@ mkdir -p /opt/homebrew/Library/
 # Install nix-darwin. Source: https://github.com/LnL7/nix-darwin
 echo "Installing nix-darwin"
 nix-build https://github.com/LnL7/nix-darwin/archive/master.tar.gz -A installer
+./result/bin/darwin-installer
 nix flake init -t nix-darwin --extra-experimental-features "nix-command flakes"
 sed -i '' "s/simple/$(scutil --get LocalHostName)/" flake.nix
 
